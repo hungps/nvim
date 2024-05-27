bfa776de5b573044ec46f0f9950cd3539c95c15b
Hung Pham Sy <25026496+hungps@users.noreply.github.com>
2024-05-27 22:27:18 +0700

    fix: temporary disable codeium to trying out the snippets

--- lua/plugins/extras/noice.lua
+++ lua/plugins/extras/noice.lua
@@ -31,19 +31,6 @@ return {
           },
         },
       },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-      },
     },
   },
   {
