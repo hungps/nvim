a98cc787fabf52af88ad0b257d4707b84b555e4a
Hung Pham Sy <25026496+hungps@users.noreply.github.com>
2024-09-23 15:12:33 +0700

    refactor: simplified the statusline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -6,18 +6,16 @@ return {
       content = {
         active = function()
           local MiniStatusline = require("mini.statusline")
-          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 9999 })
           local git = MiniStatusline.section_git({ trunc_width = 40 })
-          local diff = MiniStatusline.section_diff({ trunc_width = 75, icon = "" })
           local diagnostics = MiniStatusline.section_diagnostics({
             trunc_width = 75,
             icon = "",
             signs = { ERROR = "󰅚 ", WARN = "󰀪 ", INFO = "󰋽 ", HINT = "󰌶 " },
           })
-          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local filename = MiniStatusline.section_filename({ trunc_width = 9999 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 9999 })
           local location = MiniStatusline.section_location({ trunc_width = 75 })
-          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
           return MiniStatusline.combine_groups({
             { hl = mode_hl, strings = { mode } },
@@ -25,9 +23,9 @@ return {
             "%<", -- Mark general truncate point
             { hl = "MiniStatuslineFilename", strings = { filename } },
             "%=", -- End left alignment
-            { hl = "MiniStatuslineFilename", strings = { diagnostics, diff } },
+            { hl = "MiniStatuslineFilename", strings = { diagnostics } },
             { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-            { hl = mode_hl, strings = { search, location } },
+            { hl = mode_hl, strings = { location } },
           })
         end,
       },
@@ -61,7 +59,6 @@ return {
           fixme = hi_words({ "FIXME" }, "MiniHipatternsFixme"),
           hack = hi_words({ "HACK" }, "MiniHipatternsHack"),
           note = hi_words({ "NOTE" }, "MiniHipatternsNote"),
-          trailspace = { pattern = "%f[%s]%s*$", group = "MiniHipatternsFixme" },
           censor = {
             pattern = {
               "password:()%S+()",
@@ -97,8 +94,14 @@ return {
     "stevearc/dressing.nvim",
     opts = {
       input = {
-        enabled = false,
         start_in_insert = false,
+      },
+      select = {
+        builtin = {
+          win_options = {
+            winhighlight = "Normal:NormalFloat,CursorLine:PmenuSel",
+          },
+        },
       },
     },
   },
