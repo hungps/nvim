f90dbcaf9af69f37ce2ae821638e64cbaa4cb2e2
Hung Pham Sy <25026496+hungps@users.noreply.github.com>
2024-11-21 17:48:16 +0700

    feat: add snacks

---
+++ lua/plugins/extras/snacks.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          require("snacks").lazygit.open()
+        end,
+      },
+    },
+    opts = {
+      bigfile = { enabled = true },
+      lazygit = { enabled = true },
+      quickfile = { enabled = true },
+      rename = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesActionRename",
+        callback = function(event)
+          Snacks.rename.on_rename_file(event.data.from, event.data.to)
+        end,
+      })
+    end,
+  },
+}
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -84,14 +84,4 @@ return {
       },
     },
   },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = { "LazyGit" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Lazy[G]it" },
-    },
-  },
 }
