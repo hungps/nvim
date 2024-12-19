e15d63b1a900211d149e0f75bb1dac1049f1fae2
Hung Pham Sy <25026496+hungps@users.noreply.github.com>
2024-12-19 09:37:14 +0700

    feat: replace CopilotChat.nvim with codecompanion.nvim

--- lua/plugins/extras/copilot.lua
+++ lua/plugins/extras/copilot.lua
@@ -28,77 +28,38 @@ return {
     end,
   },
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
-    build = "make tiktoken",
+    "olimorris/codecompanion.nvim",
     dependencies = {
-      { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim" },
+      { "nvim-treesitter/nvim-treesitter" },
+      { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
+    },
+    keys = {
       {
-        "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-        opts = {
-          file_types = { "markdown", "copilot-chat" },
-        },
+        "<leader>ac",
+        "<Cmd>CodeCompanionChat<CR>",
+        desc = "[C]hat",
+        mode = "n",
       },
-    },
-    keys = function()
-      local chat = require("CopilotChat")
-      local actions = require("CopilotChat.actions")
-      local selection = require("CopilotChat.select").visual
-
-      return {
-        {
-          "<leader>ac",
-          function()
-            chat.toggle()
-          end,
-          desc = "[C]hat",
-          mode = "n",
-        },
-        {
-          "<leader>aq",
-          function()
-            vim.ui.input({ prompt = "Quick Chat: " }, function(input)
-              if input ~= "" then
-                chat.reset()
-                chat.ask(input, { selection = selection })
-              end
-            end)
-          end,
-          desc = "[Q]uick ask",
-          mode = "v",
-        },
-        {
-          "<leader>aa",
-          function()
-            require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions({ selection = selection }))
-          end,
-          desc = "[A]ctions",
-          mode = { "n", "v" },
-        },
-      }
-    end,
-    opts = {
-      model = "claude-3.5-sonnet",
-      auto_insert_mode = true,
-      highlight_headers = false,
-      window = {
-        width = 0.3,
-      },
-    },
-    config = function(_, opts)
-      local chat = require("CopilotChat")
-
-      chat.setup(opts)
-
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "copilot-*",
-        callback = function()
-          vim.keymap.set("n", "<C-r>", chat.reset, { buffer = true, remap = true, desc = "Reset chat" })
-          vim.keymap.set("n", "<Esc>", chat.close, { buffer = true, remap = true, desc = "Close the chat window" })
+      {
+        "<leader>aq",
+        function()
+          vim.ui.input({ prompt = "Quick Chat: " }, function(input)
+            if input ~= "" then
+              vim.cmd("'<,'>CodeCompanion " .. input)
+            end
+          end)
         end,
-      })
-    end,
+        desc = "[Q]uick ask",
+        mode = "v",
+      },
+      {
+        "<leader>aa",
+        "<Cmd>CodeCompanionActions<CR>",
+        desc = "[A]ctions",
+        mode = { "n", "v" },
+      },
+    },
+    config = {},
   },
 }
